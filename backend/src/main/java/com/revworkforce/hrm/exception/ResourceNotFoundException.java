c236bb40fff5d85a028e3f93972e0da3bc378778
Saikrishna1409 <nyskrishna004@gmail.com>
2026-02-26 17:13:38 +0530

    added leave and notification backend

--- backend/src/main/java/com/revworkforce/hrm/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.revworkforce.hrm.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/revworkforce/hrm/repository/AnnouncementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.revworkforce.hrm.repository;
-
-import com.revworkforce.hrm.entity.Announcement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
-}
